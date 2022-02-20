07aa11f62830e0ce0926249145a5a807aeb1443b
MissVeronica <veronica@tranza.se>
2022-02-20 16:31:04 +0100

    Update source_trace.php

--- source_trace.php
+++ source_trace.php
@@ -8,21 +8,31 @@ add_action( "update_option_{$wpdb->prefix}user_roles", 'my_um_role_update_name',
         global $wpdb;
         
         if( $option == $wpdb->prefix . 'user_roles' ) {
+            $trace_log = ini_get( 'error_log' );
+            if( empty( $trace_log ) || ini_get( 'log_errors' ) == 'Off' ) {
+                $trace_log = WP_CONTENT_DIR . '/umroles.html';
+                $html_div = '<div>';
+                $html_end = '</div>';
+            } else {
+                $html_div = '';
+                $html_end = chr(13);
+            }
+            $pretext = $html_div . '[' . wp_date( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ), time()) . '] PHP Notice: Trace rename of UM role: ';
             foreach( $old_value as $key => $value ) {                
-                if( substr( $key, 0, 3 ) == 'um_' ) {                    
-                    $output = '<div>' . $key . ' old: ' . $old_value[$key]['name'] . ' new: ' . $new_value[$key]['name'];
-                    $slug = substr( $key, 3 ); 
-                    $umrole = get_option( "um_role_{$slug}_meta" );
-                    $output .= ' meta: ' . $umrole['name'] . '</div>';
-                    file_put_contents( WP_CONTENT_DIR . '/umroles.html', $output, FILE_APPEND );
+                if( substr( $key, 0, 3 ) == 'um_' ) {                                     
+                    $output = $pretext . "'" . $key . "' Name WP old: '" . $old_value[$key]['name'] . "' Name WP new: '" . $new_value[$key]['name'] . "' ";                    
                     if( $old_value[$key]['name'] != $new_value[$key]['name'] ) {
                         $slug = substr( $key, 3 );                        
                         $umrole = get_option( "um_role_{$slug}_meta" );
+                        $output .= "Name UM meta: '" . $umrole['name'] . "' ";
                         if( $umrole['name'] == $old_value[$key]['name'] || empty( $umrole['name'] )) {
                             $umrole['name'] =  $new_value[$key]['name'];
-                            update_option( "um_role_{$slug}_meta", $umrole, true );
-                        }
-                    }
+                            $return = update_option( "um_role_{$slug}_meta", $umrole, true );
+                            if( $return ) $output .= 'UPDATED'; else $output .= 'UPDATE ERROR';
+                        } else $output .= 'old WP name not equal to current UM name, NOT UPDATED';
+                    } else $output .= 'NO ACTION';
+
+                    file_put_contents( $trace_log, $output . $html_end, FILE_APPEND );
                 }
             }
         }        
